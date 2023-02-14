3d8d331102615bd6d5cca7b2ecb68f82848b92b9
Sahar Osmani <saharosmani2003@gmail.com>
2023-02-15 09:50:23 +1100

    Initial Commit

---
+++ githubtest/githubtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtestApp.swift
+//  githubtest
+//
+//  Created by Sahar Asad Osmani on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
